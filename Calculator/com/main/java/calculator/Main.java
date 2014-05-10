9d9e145fa2484b6ee1af2ab5fafb713477520968
Eugen Mozharovsky <mozharovsky@live.com>
2014-05-10 19:59:37 +0400

    Added quadratic stuff support (issue #1) and optimization work while creating an instance of Body type.

--- Calculator/com/main/java/calculator/Main.java
+++ Calculator/com/main/java/calculator/Main.java
@@ -1,5 +1,6 @@
 package com.main.java.calculator;
 
+import java.util.ArrayList;
 import com.main.java.calculator.Model.QuadraticEquation;
 
 /**
@@ -7,7 +8,7 @@ import com.main.java.calculator.Model.QuadraticEquation;
  */
 public class Main {
     public static void main(String[] args) {
-        String s = "25 - 25 + 9981 - 88k";
+        String s = "5x^2 + 4x + 5";
         QuadraticEquation one = new QuadraticEquation(s);
 
         one.printLists();
--- Calculator/com/main/java/calculator/Model/Body.java
+++ Calculator/com/main/java/calculator/Model/Body.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 public class Body {
     private String str = null;
     private ArrayList<String> nums = new ArrayList<String>();
+    private ArrayList<String> quadraticMems = new ArrayList<String>();
     private ArrayList<String> unknownMems = new ArrayList<String>();
     private ArrayList<Integer> knownMems = new ArrayList<Integer>();
 
@@ -29,23 +30,27 @@ public class Body {
     }
 
     private void findComplexNumbers() {
+        boolean isTouched = false;
         String result = "";
 
         for(int i = 0; i < str.length(); i++) {
             if((i + 1) < str.length() && Character.isDigit(str.charAt(i)) && Character.isDigit(str.charAt(i + 1))) {
                 result += Integer.parseInt(Character.toString(str.charAt(i)));
+                isTouched = true;
             } else if((i - 1) >= 0 && Character.isDigit(str.charAt(i)) && Character.isDigit(str.charAt(i - 1))) {
                 result += Integer.parseInt(Character.toString(str.charAt(i)));
 
                 if((i + 1) < str.length() && Character.isLetter(str.charAt(i + 1))) {
                     result += Character.toString(str.charAt(i + 1));
+
+                    if((i + 3) < str.length() && str.charAt(i + 2) == '^' && str.charAt(i + 3) == '2') {
+                        result += Character.toString(str.charAt(i + 2)) + Character.toString(str.charAt(i + 3));
+                    }
                 }
 
-                /**
-                 * If you need to work with unknown mems, add code here !!!
-                 */
+                isTouched = true;
             } else {
-                if(result != "" && result != null) {
+                if(result != "^2" && result != "" && result != null) {
                     nums.add((result));
                 }
 
@@ -73,37 +78,49 @@ public class Body {
             knownMems.add(Integer.parseInt(element));
         }
 
-        for(String element : unknownMems) {
-            if(isNegative(element)) {
-                unknownMems.set(unknownMems.indexOf(element), ("-" + element));
+        if(isTouched) {
+            for(int i = 0; i < unknownMems.size(); i++) {
+                if(isNegative(unknownMems.get(i))) {
+                    unknownMems.set(i, ("-" + unknownMems.get(i)));
+                }
             }
-        }
 
-        for(int digit : knownMems) {
-            if(isNegative(Integer.toString(digit))) {
-                knownMems.set(knownMems.indexOf(digit), (-1) * digit);
+            for(int i = 0; i < knownMems.size(); i++) {
+                if(isNegative(Integer.toString(knownMems.get(i)))) {
+                    knownMems.set(i, (-1) * knownMems.get(i));
+                }
             }
-        }
 
-        nums.clear();
+            nums.clear();
+        }
     }
 
     private void findSimpleNUmbers() {
+        boolean isTouched = false;
+
         for(int i = 0; i < str.length(); i++) {
             if((i + 1) < str.length() && Character.isDigit(str.charAt(i)) && !Character.isDigit(str.charAt(i + 1))) {
-                if((i - 1) >= 0 && !Character.isDigit(str.charAt(i - 1)) || i == 0) {
+                if((i - 1) >= 0 && !Character.isDigit(str.charAt(i - 1)) && str.charAt(i - 1) != '^' || i == 0) {
                     nums.add((Character.toString(str.charAt(i))));
 
-                    if((i + 1) < str.length() && Character.isLetter(str.charAt(i + 1))) {
+                    if((i + 2) < str.length() && Character.isLetter(str.charAt(i + 1)) && str.charAt(i + 2) != '^' || (i + 2) >= str.length() && Character.isLetter(str.charAt(i + 1))) {
                         nums.set(nums.indexOf(Character.toString(str.charAt(i))), (Character.toString(str.charAt(i)) + Character.toString(str.charAt(i + 1))));
+                    } else if((i + 3) < str.length() && str.charAt(i + 2) == '^' && str.charAt(i + 3) == '2') {
+                        nums.set(nums.indexOf(Character.toString(str.charAt(i))), (Character.toString(str.charAt(i)) + Character.toString(str.charAt(i + 1)) + Character.toString(str.charAt(i + 2)) + Character.toString(str.charAt(i + 3))));
                     }
+
+                    isTouched = true;
                 }
             } else if((i + 1) >= str.length() && (i - 1) >= 0 && str.charAt(i - 1) == ' ' && Character.isDigit(str.charAt(i))) {
                 nums.add((Character.toString(str.charAt(i))));
 
-                if((i + 1) < str.length() && Character.isLetter(str.charAt(i + 1))) {
+                if((i + 2) < str.length() && Character.isLetter(str.charAt(i + 1)) && (i + 2) >= str.length()) {
                     nums.set(nums.indexOf(Character.toString(str.charAt(i))), (Character.toString(str.charAt(i)) + Character.toString(str.charAt(i + 1))));
+                } else if((i + 3) < str.length() && str.charAt(i + 2) == '^' && str.charAt(i + 3) == '2') {
+                    nums.set(nums.indexOf(Character.toString(str.charAt(i))), (Character.toString(str.charAt(i)) + Character.toString(str.charAt(i + 1)) + Character.toString(str.charAt(i + 2)) + Character.toString(str.charAt(i + 3))));
                 }
+
+                isTouched = true;
             }
         }
 
@@ -117,19 +134,22 @@ public class Body {
 
         nums.removeAll(unknownMems);
 
-        for(String element : unknownMems) {
-            if(isNegative(element)) {
-                unknownMems.set(unknownMems.indexOf(element), "-" + unknownMems.get(unknownMems.indexOf(element)));
-            }
-        }
-
         for(String element : nums) {
             knownMems.add(Integer.parseInt(element));
         }
 
-        for(int digit : knownMems) {
-            if(isNegative(Integer.toString(digit))) {
-                knownMems.set(knownMems.indexOf(digit), (-1) * knownMems.get(knownMems.indexOf(digit)));
+        if(isTouched) {
+            for(String element : unknownMems) {
+                if(isNegative(element)) {
+                    unknownMems.set(unknownMems.indexOf(element), "-" + unknownMems.get(unknownMems.indexOf(element)));
+                }
+            }
+
+
+            for(int digit : knownMems) {
+                if(isNegative(Integer.toString(digit))) {
+                    knownMems.set(knownMems.indexOf(digit), (-1) * knownMems.get(knownMems.indexOf(digit)));
+                }
             }
         }
 
@@ -182,19 +202,24 @@ public class Body {
             }
         }
 
-        if(iterationsCountOnRegion(region) > 0) {
+        if(iterationsCountOnRegion(region) > 1) {
+            region = region.replace("^2", "");
             str = str.replaceFirst(region, "");
         }
 
         return status;
     }
 
-    public void optimize() {
-        /**
-         * Digits
-         */
+    /**
+     * A prototype
+     */
+    private void optimize() {
+        optimizeKnownMems();
+        optimizeUnknownMems();
+    }
 
-        if(!knownMems.isEmpty()) {
+    private void optimizeKnownMems() {
+        if(!knownMems.isEmpty() && knownMems.size() > 1) {
             int result = knownMems.get(0);
             for(int i = 0; i < knownMems.size(); i++) {
                 if((i + 1) < knownMems.size()) {
@@ -205,19 +230,20 @@ public class Body {
             knownMems.clear();
             knownMems.add(result);
         }
+    }
 
-        /**
-         * Unknowns
-         */
-
-        if(!unknownMems.isEmpty()) {
+    private void optimizeUnknownMems() {
+        if(!unknownMems.isEmpty() && unknownMems.size() > 1) {
             boolean status = true;
             String commonChar = "";
             String tmp = "";
 
             for(String element : unknownMems) {
                 for(int i = 0; i < element.length(); i++) {
-                    if(Character.isLetter(element.charAt(i))) {
+                   if((i + 2) < element.length() && Character.isLetter(element.charAt(i)) && element.charAt(i + 1) == '^' && element.charAt(i + 2) == '2') { /** a standard quadratic form */
+                        tmp = commonChar;
+                        commonChar = Character.toString(element.charAt(i));
+                    } else if(Character.isLetter(element.charAt(i))) {
                         tmp = commonChar;
                         commonChar = Character.toString(element.charAt(i));
 
@@ -228,22 +254,71 @@ public class Body {
                 }
             }
 
+            // FIXME: unknown optimizing
             if(status) {
                 for(int i = 0; i < unknownMems.size(); i++) {
-                    unknownMems.set(i, unknownMems.get(i).replace(commonChar, ""));
-                }
-
-                int result = Integer.parseInt(unknownMems.get(0));
-                for(int i = 0; i < unknownMems.size(); i++) {
-                    if((i + 1) < unknownMems.size()) {
-                        result += Integer.parseInt(unknownMems.get(i + 1));
+                    for(int j = 0; j < unknownMems.get(i).length(); j++) {
+                        if((j + 2) < unknownMems.get(i).length()  && unknownMems.get(i).charAt(j + 1) == '^' && unknownMems.get(i).charAt(j + 2) == '2') { /** a standard quadratic form */
+                            unknownMems.set(i, unknownMems.get(i).replace(commonChar + "^2", ""));
+                            quadraticMems.add(unknownMems.get(i));
+                        } else if((j + 1) >= unknownMems.get(i).length() && Character.isLetter(unknownMems.get(i).charAt(j)) && Character.isDigit(unknownMems.get(i).charAt(j - 1))) {
+                            unknownMems.set(i, unknownMems.get(i).replace(commonChar, ""));
+                        }
                     }
                 }
 
-                unknownMems.clear();
-                unknownMems.add(Integer.toString(result) + commonChar);
+                // To remove all iterated elements
+                prepareUnknownMems();
+
+                optimizeQuadraticMems(commonChar);
+
+                if(unknownMems.size() > 0) {
+                    // Unknown mems optimization
+                    int result = Integer.parseInt(unknownMems.get(0));
+                    for(int i = 0; i < unknownMems.size(); i++) {
+                        if((i + 1) < unknownMems.size()) {
+                            result += Integer.parseInt(unknownMems.get(i + 1));
+                        }
+                    }
+
+                    unknownMems.clear();
+                    unknownMems.add(Integer.toString(result) + commonChar);
+                }
+
+                unknownMems.addAll(quadraticMems);
+                quadraticMems.clear();
             } else {
                 System.out.println("Failed to load unknown mems ...");
+            }
+        }
+    }
+
+    private void optimizeQuadraticMems(String commonChar) {
+        if(!quadraticMems.isEmpty() && quadraticMems.size() > 1) {
+            int result = Integer.parseInt(quadraticMems.get(0));
+            for(int i = 0; i < quadraticMems.size(); i++) {
+                if((i + 1) < quadraticMems.size()) {
+                    result += Integer.parseInt(quadraticMems.get(i + 1));
+                }
+            }
+
+            quadraticMems.clear();
+            quadraticMems.add(Integer.toString(result) + commonChar + "^2");
+        } else if(!quadraticMems.isEmpty()) {
+            quadraticMems.set(0, quadraticMems.get(0) + commonChar + "^2");
+        }
+    }
+
+    private void prepareUnknownMems() {
+        ArrayList<String> copy = new ArrayList<String>();
+        copy.addAll(quadraticMems);
+
+        for(int i = 0; i < unknownMems.size(); i++) {
+            for(int j = 0; j < copy.size(); j++) {
+                if(unknownMems.get(i).equals(copy.get(j))) {
+                    unknownMems.remove(copy.get(j));
+                    copy.remove(copy.get(j));
+                }
             }
         }
     }
